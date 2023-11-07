c4c33a4fd6af8a5c0457fd0ed809e1ae73ef8fec
jorge-marcelo <jorge3258@hotmail.com>
2023-11-06 23:02:52 -0300

    Alterações extras

--- js/animScroll.js
+++ js/animScroll.js
@@ -15,3 +15,19 @@ elemento.classList.remove(animationClass);
 
 window.addEventListener("scroll", animScroll);
 animScroll();
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+anchor.addEventListener("click", function (e) {
+e.preventDefault();
+
+const targetId = this.getAttribute("href").substring(1);
+const targetElement = document.getElementById(targetId);
+
+if (targetElement) {
+window.scrollTo({
+top: targetElement.offsetTop,
+behavior: "smooth",
+});
+}
+});
+});
