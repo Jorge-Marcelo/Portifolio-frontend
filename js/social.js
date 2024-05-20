1a5ccac83b2abb9725cc25928cfbf7380c16b06c
Jorge Marcelo <49494259+Jorge-Marcelo@users.noreply.github.com>
2024-05-20 15:51:38 -0300

    Adicionando link de  Curriculo atualizado

--- js/social.js
+++ js/social.js
@@ -47,7 +47,7 @@ github(githubLink);
 });
 
 const curriculoLink =
-"https://drive.google.com/file/d/1BWnxT8CJ7rkMcIGrllk6VZ0FbuVSTyli/view?usp=sharing";
+"https://drive.google.com/file/d/11cx44eraMtCmqwnHdp_y28HkVO-of7j9/view?usp=sharing";
 
 const CVButton = document.getElementById("curriculo");
 function curriculo(curriculoLink) {
